cbb526c6ad2e80b2aa757233c271eb97c9aac2f9
Viacheslav Zhygulin <s.zhygulin@gmail.com>
2019-06-17 16:31:38 +0300

    added functions

--- es-blockchain.py
+++ es-blockchain.py
@@ -160,5 +160,5 @@ if __name__ == '__main__':
     #initiate()
     setUserBalance("user1", "USDAsset", 10)
     setUserBalance("user2", "EnergyAsset", 10)
-    trade("user2", "user1", 1, 1)
+    #trade("user2", "user1", 1, 1)
     print(getUserBalances("user1"))
